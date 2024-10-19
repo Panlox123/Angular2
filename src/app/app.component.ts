3e93a8d04b43d0dfdc7fb0db21c71f21cf9a3c08
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2024-10-19 06:06:10 -0500

    act2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto3';
+  title : string = 'proyecto "Reparacion de Pistas"';
+  montoInversion: number | null = null;
+  laMar: number = 0;
+  abancay: number = 0;
+  julio: number = 0;
+  aviacion: number = 0;
+  tacna: number = 0;
+
+  calcularReparticion() {
+    if (this.montoInversion !== null) {
+      this.laMar = this.montoInversion * 0.35; // 35%
+      this.abancay = this.montoInversion * 0.25; // 25%
+      this.julio = this.montoInversion * 0.10; // 10%
+      this.aviacion = this.montoInversion * 0.15; // 15%
+      this.tacna = this.montoInversion - (this.laMar + this.abancay + this.julio + this.aviacion); // Resto
+    }
+  }
 }
